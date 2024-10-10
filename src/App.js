596315f2bc2a46bfaad04211b6bf0643db93150b
Mena Ehab <127118141+commanderYT123@users.noreply.github.com>
2024-10-10 15:59:10 +0300

    Install react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
+import About from "./components/About";
 import Carousel from "./components/Carousel";
 import NavBar from "./components/NavBar";
 import ProductsList from "./components/ProductsList";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Carousel />
-      <ProductsList />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Carousel />
+              <ProductsList />
+            </>
+          }
+        />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
